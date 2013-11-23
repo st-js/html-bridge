b4e277f1038706b05ed897eae47fdf006de6dfff
npiguet <npiguet@romandie.com>
2013-11-23 16:22:49 +0100

    moved more methods from Global to the st-js.shared JSGlobal object. These methods belong to the language definition, not to the HTML bridge.

--- src/main/java/org/stjs/javascript/Global.java
+++ src/main/java/org/stjs/javascript/Global.java
@@ -15,10 +15,6 @@
  */
 package org.stjs.javascript;
 
-import org.stjs.javascript.annotation.GlobalScope;
-import org.stjs.javascript.annotation.Template;
-import org.stjs.javascript.functions.Callback0;
-
 @GlobalScope
 public class Global extends JSGlobal {
 	public static Window window;
@@ -31,75 +27,35 @@ public class Global extends JSGlobal {
 	// do not add this one too
 	// public static Window self;
 
-	public native static <T> T eval  (String expr);
+	public native static void alert(Object expr);
 
-	public native static void alert  (Object expr);
+	public native static boolean confirm(String expr);
 
-	public native static boolean confirm  (String expr);
+	public native static String prompt(String expr);
 
-	public native static String prompt  (String expr);
+	public native static String prompt(String expr, String defaultText);
 
-	public native static String prompt  (String expr, String defaultText);
+	public native static TimeoutHandler setTimeout(String expr, int timeoutMillis);
 
-	public native static double parseFloat  (Object expr);
+	public native static TimeoutHandler setTimeout(Callback0 expr, int timeoutMillis);
 
-	public native static int parseInt  (Object expr);
+	public native static TimeoutHandler setInterval(String expr, int timeoutMillis);
 
-	public native static int parseInt  (Object expr, int radix);
+	public native static TimeoutHandler setInterval(Callback0 expr, int timeoutMillis);
 
-	@Template("typeOf")
-	public native static String typeof  (Object obj);
+	public native static void clearTimeout(TimeoutHandler handler);
 
-	/**
-	 * this is the equivalent of x || y || z in javascript
-	 * 
-	 * @return
-	 */
-	@Template("or")
-	public native static <T> T $or  (T value, T... otherValues);
-
-	public native static TimeoutHandler setTimeout  (String expr, int timeoutMillis);
-
-	public native static TimeoutHandler setTimeout  (Callback0 expr, int timeoutMillis);
-
-	public native static TimeoutHandler setInterval  (String expr, int timeoutMillis);
-
-	public native static TimeoutHandler setInterval  (Callback0 expr, int timeoutMillis);
-
-	public native static void clearTimeout  (TimeoutHandler handler);
-
-	public native static void clearInterval  (TimeoutHandler handler);
-
-	public native static String decodeURI  (String uri);
-
-	public native static String decodeURIComponent  (String uri);
-
-	public native static String encodeURI  (String uri);
-
-	public native static String encodeURIComponent  (String uri);
-
-	public native static String escape  (String uri);
-
-	public native static boolean isFinite  (Object value);
-
-	public native static boolean isNaN  (Object value);
-
-	public native static String unescape  (String uri);
+	public native static void clearInterval(TimeoutHandler handler);
 
 	/**
-	 * defined in stjs.js
-	 * 
-	 * @param exception
-	 * @return
+	 * Deprecated, use encodeURI() or encodeURIComponent() instead.
 	 */
-	public native static RuntimeException exception  (Object exception);
+	@Deprecated
+	public native static String escape(String uri);
 
 	/**
-	 * defined in stjs.js
-	 * 
-	 * @param obj
-	 * @return
+	 * Deprecated, use decodeURI() or decodeURIComponent() instead.
 	 */
-	public native static boolean isEnum  (Object obj);
-
+	@Deprecated
+	public native static String unescape(String uri);
 }
