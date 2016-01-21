ed940043699a812e8f7a63de7c8ff9dba79840e9
Alex Zhukov <zhukov.alex@gmail.com>
2016-01-21 20:09:13 +0200

    AnimationFrame

--- src/main/java/org/stjs/javascript/Window.java
+++ src/main/java/org/stjs/javascript/Window.java
@@ -18,6 +18,7 @@ package org.stjs.javascript;
 import org.stjs.javascript.dom.DOMEvent;
 import org.stjs.javascript.dom.Document;
 import org.stjs.javascript.dom.EventTarget;
+import org.stjs.javascript.functions.Callback0;
 import org.stjs.javascript.functions.Callback1;
 import org.stjs.javascript.functions.Function1;
 import org.w3c.dom.events.Event;
@@ -101,4 +102,8 @@ public class Window implements EventTarget {
 
 	@Override
 	public native boolean dispatchEvent(DOMEvent event);
+
+	public native void cancelAnimationFrame(int animationRequestId);
+
+	public native int requestAnimationFrame(Callback0 callback);
 }
