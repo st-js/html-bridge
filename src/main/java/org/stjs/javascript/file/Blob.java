542fe6071d6b38a20d14851f376d71cd2546c4df
Alex Zhukov <zhukov.alex@gmail.com>
2016-02-10 13:25:34 +0200

    websocket

---
+++ src/main/java/org/stjs/javascript/file/Blob.java
@@ -0,0 +1,24 @@
+package org.stjs.javascript.file;
+
+import org.stjs.javascript.annotation.STJSBridge;
+
+@STJSBridge
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Blob
+ * 
+ * A Blob object represents a file-like object of immutable, raw data. Blobs
+ * represent data that isn't necessarily in a JavaScript-native format. The File
+ * interface is based on Blob, inheriting blob functionality and expanding it to
+ * support files on the user's system.
+ * 
+ * To construct a Blob from other non-blob objects and data, use the Blob()
+ * constructor. To create a blob that contains a subset of another blob's data,
+ * use the slice() method. To obtain a Blob object for a file on the user's file
+ * system, see the File documentation.
+ * 
+ * The APIs accepting Blob objects are also listed on the File documentation.
+ *
+ */
+public class Blob {
+
+}
