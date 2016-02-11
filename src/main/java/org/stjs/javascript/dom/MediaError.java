c2fc0460ea87d191f767cef85c65216bb70707fc
Alex Zhukov <zhukov.alex@gmail.com>
2016-02-11 11:11:24 +0200

    media error constants

--- src/main/java/org/stjs/javascript/dom/MediaError.java
+++ src/main/java/org/stjs/javascript/dom/MediaError.java
@@ -1,6 +1,10 @@
 package org.stjs.javascript.dom;
 
 public class MediaError {
+    public final int MEDIA_ERR_ABORTED = 1;
+    public final int MEDIA_ERR_NETWORK = 2;
+    public final int MEDIA_ERR_DECODE = 3;
+    public final int MEDIA_ERR_SRC_NOT_SUPPORTED = 4;
 
     public int code;
 
