bf1453b9a4e7a0016508fc0ca5b6de1425b60765
Alex Zhukov <zhukov.alex@gmail.com>
2016-02-11 15:44:00 +0200

    media error javadoc

--- src/main/java/org/stjs/javascript/dom/MediaError.java
+++ src/main/java/org/stjs/javascript/dom/MediaError.java
@@ -1,9 +1,25 @@
 package org.stjs.javascript.dom;
 
 public class MediaError {
+
+    /**
+     * The fetching process for the media resource was aborted by the user agent
+     * at the user's request.
+     */
     public final int MEDIA_ERR_ABORTED = 1;
+    /**
+     * A network error of some description caused the user agent to stop
+     * fetching the media resource, after the resource was established to be
+     * usable.
+     */
     public final int MEDIA_ERR_NETWORK = 2;
+    /**
+     * An error of some description occurred while decoding the media resource,
+     * after the resource was established to be usable.
+     */
     public final int MEDIA_ERR_DECODE = 3;
+
+    /** The media resource indicated by the src attribute was not suitable. */
     public final int MEDIA_ERR_SRC_NOT_SUPPORTED = 4;
 
     public int code;
