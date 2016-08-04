a60838d090302eed84e7e2d2be812eee7afe7d1d
Aleksey Sevruk <aleksey.sevruk@gmail.com>
2016-08-04 20:27:19 +0300

    FileReader added

--- src/main/java/org/stjs/javascript/dom/FileReader.java
+++ src/main/java/org/stjs/javascript/dom/FileReader.java
@@ -1,7 +1,20 @@
 package org.stjs.javascript.dom;
 
-/**
- * Created by aleksey on 8/3/16.
- */
+import org.stjs.javascript.functions.Callback1;
+import org.stjs.javascript.functions.Function1;
+import org.w3c.dom.DOMError;
+
 public class FileReader {
+    public DOMError error;
+    public String readyState;
+    public String result;
+
+    public native void readAsDataURL(File file);
+
+    public Function1<DOMEvent, Boolean> onabort;
+    public Function1<DOMEvent, Boolean> onerror;
+    public Function1<DOMEvent, Boolean> onload;
+    public Function1<DOMEvent, Boolean> onloadstart;
+    public Callback1<DOMEvent> onloadend;
+    public Function1<DOMEvent, Boolean> onprogress;
 }
